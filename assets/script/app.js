807501bb34b3e13bec195497b81f1cecbe648b9c
Stefano <stefano.turcarelli@icloud.com>
2023-12-02 16:53:00 -0600

    Change style

--- assets/script/app.js
+++ assets/script/app.js
@@ -12,3 +12,15 @@ import {
 	create,
 } from "./utils.js";
 
+const clockDisplay = select(".clock-display");
+
+const clock = () => {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds().toString().padStart(2, "0");
+
+  clockDisplay.innerHTML = `${hours}:${minutes}:${seconds}`;
+};
+
+setInterval(clock, 1000);
