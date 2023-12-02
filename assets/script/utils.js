5210315cc1153a355a6e6a4329205b51a7bb1298
Stefano <stefano.turcarelli@icloud.com>
2023-12-02 15:29:36 -0600

    Change style

--- assets/script/utils.js
+++ assets/script/utils.js
@@ -1,21 +1,13 @@
 "use strict";
 
-/**
- * Utility functions
- * OnEvent
- * getElement (by id)
- * select
- * selectAll
- * print
- * sleep
- * randomNumber
- * filterArray
- * create
- */
-
 // Add event listener
 function onEvent(event, selector, callback) {
   return selector.addEventListener(event, callback);
+}
+
+// Get HTML element by id
+function getElement(selector, parent = document) {
+  return parent.getElementById(selector);
 }
 
 // Select HTML element
@@ -28,6 +20,11 @@ function selectAll(selector, parent = document) {
   return [...parent.querySelectorAll(selector)];
 }
 
+// Print
+function print(arg) {
+  console.log(arg);
+}
+
 // Sleep
 function sleep(duration) {
   return new Promise((resolve) => {
@@ -35,21 +32,29 @@ function sleep(duration) {
   });
 }
 
+// Generate random number between - and including - 'min' and 'max'
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// Filter array
+function filterArray(array, callback) {
+  return array.filter(callback);
+}
+
 // Create an HTML element
 function create(element, parent = document) {
   return parent.createElement(element);
 }
 
-// Display text
-function displayText(text, output) {
-  let newParagraph = document.createElement("p");
-  newParagraph.appendChild(document.createTextNode(text));
-  return output.appendChild(newParagraph);
-}
-
-// Clean output
-function cleanOutput(output) {
-  output.innerHTML = "";
-}
-
-export { onEvent, select, selectAll, sleep, create, displayText, cleanOutput };
+export {
+  onEvent,
+  getElement,
+  select,
+  selectAll,
+  print,
+  sleep,
+  randomNumber,
+  filterArray,
+  create,
+};
